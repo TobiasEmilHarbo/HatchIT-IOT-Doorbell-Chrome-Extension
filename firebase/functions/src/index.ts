0df982f1fa8ccbc0d2b41a90c7e63486e71438d2
TobiasEmil <tobias@protofi.dk>
2019-08-16 17:02:58 +0200

    firebase integration added

---
+++ firebase/functions/src/index.ts
@@ -0,0 +1,18 @@
+import * as functions from 'firebase-functions';
+import * as admin from 'firebase-admin'
+
+try{
+    admin.initializeApp()
+}
+catch(e){ console.log(e.message) }
+
+const settings = { timestampsInSnapshots: true }
+
+const adminFs = admin.firestore()
+adminFs.settings(settings)
+
+exports.doorbell = functions.pubsub.topic('doorbell').onPublish(async message => {
+    return admin.firestore().collection('notifications').add({
+        notify : true
+    })
+})
